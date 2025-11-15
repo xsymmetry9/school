ae65eef3e8153cda5a11979e58c181bfa6534b13
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-11-16 00:31:37 +0800

    Add articles index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :student_progress_reports
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 root 'pages#home'
 get 'about', to:'pages#about'
 
