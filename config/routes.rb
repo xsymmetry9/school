b3fd7db33b81882cfdf7bd55ed1b96ba38cff089
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-11-16 00:17:07 +0800

    Work on Article from scratch

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :student_progress_reports
-  resources :articles
+  resources :articles, only: [:show]
 root 'pages#home'
 get 'about', to:'pages#about'
+
 end
