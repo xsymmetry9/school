62974526a7e4ba178a04449b0b6a9003b855bccb
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-11-19 23:04:32 +0800

    Add sign up function

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,6 @@ class  User < ApplicationRecord
     has_secure_password
     before_save {self.email = email.downcase}
     has_many :articles
-    validates :username, presence: true, 
-                                    uniqueness: {case_sensitive: false}, 
-                                    length: {minimum: 3, maximum: 25}
-    validates :email, presence: true, 
-                                    format: {with: URI::MailTo::EMAIL_REGEXP },
-                                    uniqueness: {case_sensitive: false}, 
-                                    length: {maximum: 105}
+    validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+    validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP }, uniqueness: {case_sensitive: false}, length: {maximum: 105}
 end
