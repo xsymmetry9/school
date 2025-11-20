f75cd3e8d1e4beae48ba0c4524ef8f5dc4341348
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-11-20 09:46:21 +0800

    Add link to articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
         @articles = @user.articles
     end
 
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
     end
@@ -16,7 +20,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if@user.update(user_params)
             flash[:notice] = "Your account has been successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit', status: :unprocessable_entity
         end
