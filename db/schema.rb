916efbfa2c43d9efe2a65dda67952eef68a144c2
Gary-mac <glswim@gmail.com>
2025-11-20 19:29:23 +0800

    Add admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_19_130406) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_20_111059) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -35,5 +35,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_19_130406) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 end
