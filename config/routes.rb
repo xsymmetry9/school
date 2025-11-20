03deb3051dacaec5fba07ebc44b6adcb9bd0f376
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-11-20 13:14:29 +0800

    Add more restriction

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ get 'about', to:'pages#about'
 
 get 'signup', to: 'users#new'
 resources :users, except: [:new]
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
 end
